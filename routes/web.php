310366254781b8fcd7492ca7dd089da9acc70b7e
detossj <jorgerubilarssj@gmail.com>
2025-06-03 20:24:09 -0400

    Routes added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/api/peliculas', [PeliculasController::class,'get']);
+Route::get('/api/peliculas/{$id}', [PeliculasController::class,'getById']);
+Route::post('/api/peliculas', [PeliculasController::class,'create']);
+Route::put('/api/peliculas/{$id}', [PeliculasController::class,'update']);
+Route::update('/api/peliculas/{$id}', [PeliculasController::class,'delete']);
