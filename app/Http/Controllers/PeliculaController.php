892b85b79f9b5a4d60aa5837cd62d73a743bd313
detossj <jorgerubilarssj@gmail.com>
2025-06-03 20:20:02 -0400

    functions added

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -7,5 +7,55 @@ use Illuminate\Http\Request;
 
 class PeliculaController extends Controller
 {
-    //
+    public function get(){
+        
+        $peliculas = Pelicula::all();
+        return response()->json($peliculas);
+    }
+
+    public function getById(){
+
+        $pelicula = Pelicula::find($id);
+
+        if($pelicula){
+            return response()->json($pelicula);
+        } 
+        else {
+            return response()->json(['message' => 'Pelicula no encontrada'], 404);
+        }
+    }
+
+    public function create(Request $request){
+
+        $pelicula = Pelicula::create($request->all());
+
+        return response()->json($pelicula, 201);
+    }
+
+    public function update(Request $request, $id){
+
+        $pelicula = Pelicula::find($id);
+
+        if($pelicula){
+            $pelicula->update($request->all());
+            return response()->json($pelicula);
+        }
+        else {
+            return response()->json(['message' => 'Pelicula no encontrada'], 404);
+        }
+    }
+
+    public function delete($id){
+
+        $pelicula = Pelicula::find($id);
+
+        if($pelicula){
+            $pelicula->delete();
+            return response()->json(['message' => 'Pelicula eliminada']);
+        }
+        else {
+            return response()->json(['message' => 'Pelicula no encontrada'], 404);
+        }
+
+    }
 }
