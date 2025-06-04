ec941fbba0933dc7c6cb0ffacc51d73b4c1343c8
detossj <jorgerubilarssj@gmail.com>
2025-06-03 20:33:17 -0400

    library scribe added and web.php fix

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/api/peliculas', [PeliculasController::class,'get']);
-Route::get('/api/peliculas/{$id}', [PeliculasController::class,'getById']);
-Route::post('/api/peliculas', [PeliculasController::class,'create']);
-Route::put('/api/peliculas/{$id}', [PeliculasController::class,'update']);
-Route::update('/api/peliculas/{$id}', [PeliculasController::class,'delete']);
+Route::get('/api/peliculas', [PeliculaController::class,'get']);
+Route::get('/api/peliculas/{$id}', [PeliculaController::class,'getById']);
+Route::post('/api/peliculas', [PeliculaController::class,'create']);
+Route::put('/api/peliculas/{$id}', [PeliculaController::class,'update']);
+Route::delete('/api/peliculas/{$id}', [PeliculaController::class,'delete']);
