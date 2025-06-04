a08ecb4115d3d2934fc10fd755224440fd580513
detossj <jorgerubilarssj@gmail.com>
2025-06-03 21:14:43 -0400

    error fix Class PeliculaController does not exist

--- app/Http/Controllers/PeliculaController.php
+++ app/Http/Controllers/PeliculaController.php
@@ -2,24 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Pelicula;
 
-
 class PeliculaController extends Controller
 {
-    public function get(){
-        
+    public function get() {
         $peliculas = Pelicula::all();
         return response()->json($peliculas);
     }
 
-    public function getById($id){
-
+    public function getById($id) {
         $pelicula = Pelicula::find($id);
-
-        if($pelicula){
+        if ($pelicula) {
             return response()->json($pelicula);
         } 
         else {
@@ -27,37 +22,30 @@ class PeliculaController extends Controller
         }
     }
 
-    public function create(Request $request){
-
+    public function create(Request $request) {
         $pelicula = Pelicula::create($request->all());
-
         return response()->json($pelicula, 201);
     }
 
-    public function update(Request $request, $id){
-
+    public function update(Request $request, $id) {
         $pelicula = Pelicula::find($id);
-
-        if($pelicula){
+        if ($pelicula) {
             $pelicula->update($request->all());
             return response()->json($pelicula);
-        }
+        } 
         else {
             return response()->json(['message' => 'Pelicula no encontrada'], 404);
         }
     }
 
-    public function delete($id){
-
+    public function delete($id) {
         $pelicula = Pelicula::find($id);
-
-        if($pelicula){
+        if ($pelicula) {
             $pelicula->delete();
             return response()->json(['message' => 'Pelicula eliminada']);
-        }
+        } 
         else {
             return response()->json(['message' => 'Pelicula no encontrada'], 404);
         }
-
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/peliculas/{id}', [PeliculaController::class, 'getById']);
 Route::post('/peliculas', [PeliculaController::class, 'create']);
 Route::put('/peliculas/{id}', [PeliculaController::class, 'update']);
 Route::delete('/peliculas/{id}', [PeliculaController::class, 'delete']);
+
